21b44bf9d3542bd4dae20f85421b2687051f6f54
mandlemkavya <98142665+mandlemkavya@users.noreply.github.com>
2022-04-06 18:59:27 -0500

    addmods.pug

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var jamRouter = require('./routes/jam');
+var addmodsRouter = require('./routes/addmods');
 var app = express();
 
 // view engine setup
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/jam', jamRouter);
+app.use('/addmods', addmodsRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
